dc163e9afb904513e25e8fe75413adb338d4b134
mellaniesoriano <23709723+mellaniesoriano@users.noreply.github.com>
2019-02-25 23:58:21 -1000

    Updates

---
+++ precache-manifest.c56698d99016cd236eb16e71809ac593.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "23802fab7834668506b8",
+    "url": "/hooked-on-hangman/static/js/runtime~main.23802fab.js"
+  },
+  {
+    "revision": "5db30fabd42b7cbd9cc1",
+    "url": "/hooked-on-hangman/static/js/main.5db30fab.chunk.js"
+  },
+  {
+    "revision": "aba701eda4d1b0288d39",
+    "url": "/hooked-on-hangman/static/js/2.aba701ed.chunk.js"
+  },
+  {
+    "revision": "5db30fabd42b7cbd9cc1",
+    "url": "/hooked-on-hangman/static/css/main.d6c58e9b.chunk.css"
+  },
+  {
+    "revision": "858d7ae0779bdabb521afdbedae01d9c",
+    "url": "/hooked-on-hangman/index.html"
+  }
+];
