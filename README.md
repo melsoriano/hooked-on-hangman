48e978a6f4d8baaed56a358d7f222226b51e58f1
mellaniesoriano <23709723+mellaniesoriano@users.noreply.github.com>
2019-03-09 16:31:23 -1000

    Added more words

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Overview
 
-Web development themed hangman game created for the purpose of experimenting and learning React Hooks. Fun little game to learn React Hooks. [Click here to play.](https://mellaniesori.github.io/hooked-on-hangman/)
+Web development themed hangman game created for the purpose of experimenting and learning React Hooks. Fun little game to learn React Hooks. Test your web dev/programming knowledge: [Click here to play!](https://mellaniesori.github.io/hooked-on-hangman/)
 
 ### Launch in Development
 
--- src/helpers.js
+++ src/helpers.js
@@ -7,7 +7,8 @@ export const data = [
   },
   {
     word: 'react',
-    hint: 'a front end framework known for using state'
+    hint:
+      'JavaScript frontend library used for building single page applications'
   },
   {
     word: 'angular',
@@ -24,6 +25,42 @@ export const data = [
   {
     word: 'typescript',
     hint: 'like javascript, but strongly typed'
+  },
+  {
+    word: 'cache',
+    hint:
+      'the storage of certain elements to help with faster load times from repeat website visitors.'
+  },
+  {
+    word: 'domain',
+    hint: 'address for a website as entered into the browser.'
+  },
+  {
+    word: 'firewall',
+    hint: 'system to protect a secure network from an unsecure network.'
+  },
+  {
+    word: 'framework',
+    hint: 'collective suite of programs used in development.'
+  },
+  {
+    word: 'wireframe',
+    hint: 'bare bones structure of a website or application'
+  },
+  {
+    word: 'debugging',
+    hint:
+      'the process of identifying and dealing an error that prevents a website or app from running as it should'
+  },
+  {
+    word: 'json',
+    hint:
+      'a file format that uses human-readable text to transmit data objects consisting of key-value pairs'
+  },
+  {
+    word: 'redux',
+    hint:
+      'single source of truth used for managing the state of your application.'
   }
 ];
 
